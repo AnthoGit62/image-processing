d937b2f2dfd280237111ec0e227137a5e90b69a8
louisprvst <louis.prevost1610@gmail.com>
2024-12-16 10:58:50 +0100

    On utilise un ipynb car on a un bug d'affichage sur linux

--- code_prof.py
+++
@@ -1,8 +0,0 @@
-from astropy.io import fits
-import matplotlib.pyplot as plt 
-
-data = fits.getdata('image-processing/Tarantula_Nebula-sii.fit')
-
-plt.imshow(data , cmap='gray')
-plt.colorbar()
-plt.show()
